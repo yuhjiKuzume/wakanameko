dd616e9f5164efe0b3a30eabe26e7a3f4cd1cfcd
yuhjiKuzume <Yuji.Kuzume@jp.ricoh.com>
2024-09-13 13:14:47 +0100

    ミニフィグ撮影＆送信プログラムを挿入

--- RasPike/sdk/workspace/etrobo2023/captureMinifig.py
+++ RasPike/sdk/workspace/etrobo2023/captureMinifig.py
@@ -2,22 +2,35 @@ from picamera2.picamera2 import *
 import time
 import os
  
-def picam_save():
+# 画像を撮影
+def picam_save(count):
   picam2 = Picamera2()
  
-  save_dir = "minifig"
+  # 撮影した画像ファイルを保存するディレクトリ
+  save_dir = "minifig_dir"
   if not os.path.exists(save_dir):
     os.makedirs(save_dir)
  
-  timestamp = time.strftime("%Y%m%d_%H%M%S")
-  filename = f"minifig_{timestamp}.jpg"
+  # 撮影した画像の名前
+  filename = f"minifig_{count}.jpg"
   file_path = os.path.join(save_dir, filename)
  
-  picam2.start_preview(Preview.QTGL)
+  # カメラ画角指定
+  picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (2304, 1296)}))
+  # picam2.start_preview(Preview.QTGL)
   picam2.start()
   time.sleep(2)
  
   picam2.capture_file(file_path)
   picam2.close()
  
+  # 撮影した画像の保存先パスを返す
   return file_path
+
+if __name__ == "__main__":
+  arg = int(sys.argv[1])
+  filePath = picam_save(arg)
+
+  # PCの共有フォルダに画像を送信
+  mount_dir = "/mnt/share"
+  os.system(f"sudo cp {filePath} {mount_dir}")
