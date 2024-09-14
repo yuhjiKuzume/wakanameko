4a54372691b05056f355c5de7bfc0b9d816395bf
yuhjiKuzume <Yuji.Kuzume@jp.ricoh.com>
2024-09-14 12:00:02 +0100

    captureTrain修正

--- RasPike/sdk/workspace/etrobo2023/captureTrain.py
+++ RasPike/sdk/workspace/etrobo2023/captureTrain.py
@@ -2,16 +2,25 @@ from picamera2.picamera2 import *
 import time
 import subprocess
  
-h264_file = "./video.h264"
-mp4_file = "./video.mp4"
+h264_file = "./video2.h264"
+mp4_file = "./video2.mp4"
 
+width = 1980
+length = 1080
 # 動画を撮影
 def capture_train():
   picam2 = Picamera2()
 
-  picam2.resolution = (1280, 720) # 解像度設定
+  fullReso = picam2.camera_properties['PixelArraySize']  # センサー解像度
+  aspectRatio = fullReso[ 0 ] / fullReso[ 1 ]            # アスペクト比(W/H)
+  previewReso = ( int( 1000 * aspectRatio ), 1000 )        # プレビュー表示解像度
+  camera_config = picam2.create_video_configuration(main={"size": previewReso}, raw={"size": fullReso})
   picam2.framerate = 30 # フレームレート設定
   encoder = H264Encoder(bitrate=10000000) # ビットレート設定
+  
+  # 設定を適用
+  picam2.configure(camera_config)
+  # カメラを起動
   picam2.start_recording(encoder, h264_file)
   time.sleep(10) # 10秒間録画
   picam2.stop_recording()
